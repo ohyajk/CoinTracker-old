94b32141e90c9f35ebb6889772bf4a54c52bcb00
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-02-07 15:07:40 +0530

    change import order

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './redux/configurestore';
 import './index.css';
 import App from './App';
+import store from './redux/configurestore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
